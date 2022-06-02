bb64452b490472b16a6687630ca76e328f713110
betocarreira22 <106412148+betocarreira22@users.noreply.github.com>
2022-06-02 19:50:32 -0300

    Update aula-2022-06-02.c

--- Exercícios_de_sala/aula-2022-06-02.c
+++ Exercícios_de_sala/aula-2022-06-02.c
@@ -1,1 +1,27 @@
+/*
+ler um int num1
+ler um int num2
+dimimnuir num1 com num2
+resultado=num1-num2
+imprimir resultado
+
+
+*/
+
+#include <studio.h>
+int main(){
+int num1,num2,saldo;
+printf("informe o num1:");
+scanf("%d",&num1);
+printf("informe o num2:");
+scanf("%d",&num2);
+saldo=num1-num2;
+printf("saldo:%d\n",saldo);
+
+if(saldo>2000)
+printf("Qual é o saldo positivo atual:")
+else if(saldo<500)
+printf("Qual é o saldo negativo atual:")
+
+}
 
