c863caf9b25f3a86b293389a5da628880784810b
betocarreira22 <106412148+betocarreira22@users.noreply.github.com>
2022-06-15 16:28:57 -0300

    Create exerc__cap2__7.c

---
+++ exerc__cap2__7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+int num1;
+printf("Informe o num1:");
+scanf("%d",&num1);
+
+if(num1>20 && num1<50){
+ printf("num1 esta compreendido entre 20 e 50");
+}
+else 
+printf("num1 nao esta compreendido entre 20 e 50");
+
+}
