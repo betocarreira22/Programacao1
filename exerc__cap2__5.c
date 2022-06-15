9eebeeb7d080b1c732ad8c6ad8ea6aab53840444
betocarreira22 <106412148+betocarreira22@users.noreply.github.com>
2022-06-15 16:24:09 -0300

    Create exerc__cap2__5.c

---
+++ exerc__cap2__5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () {
+    int num1;
+    printf("Informe o num1:");
+    scanf("%f",&num1);
+    
+    if(num1%3==0 && num1%7==0){
+    printf("e divisivel por 3 e por 7");
+}
+    else
+    printf("nao e divisivel por 3 e por 7");
+	}
